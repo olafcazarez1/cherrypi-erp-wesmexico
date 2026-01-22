83be2ab9027b604286e3f69845ad7593ee3de632
Olaf Cazarez <olafc@zoomcatalog.com>
2026-01-21 18:39:03 -0800

    ! minor changes

---
+++ run_wesmexico_service.sh
@@ -0,0 +1,15 @@
+SERVICE='cherrypi-wesmexico.py\s --config-file'
+SCRIPT_PATH=$(dirname `which $0`)
+
+while true
+	do
+
+		if ps -AF | grep -v grep | grep -e 'cherrypi-wesmexico.py --config-file' > /dev/null
+		then
+			sleep 2
+			continue
+		else
+			python3.12 $SCRIPT_PATH/cherrypi-wesmexico.py --config-file $SCRIPT_PATH/conf/default.conf&
+			sleep 5
+		fi
+done
